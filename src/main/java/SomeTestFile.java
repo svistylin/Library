05f50e12cef860682fd4309995167585dcbcc7d3
MKrechkivsky <mikhael.krechkivsky@intertechgroup.biz>
2021-02-16 09:48:51 +0300

    first

---
+++ src/main/java/SomeTestFile.java
@@ -0,0 +1,7 @@
+public class SomeTestFile {
+    public static void main(String[] args) {
+        System.out.println("i'm here");
+        System.out.println("Me too");
+        System.out.println("third");
+    }
+}
